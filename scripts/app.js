4480c28c49182c3c7f2acef4e0a04ed7fe5675de
TonyRod116 <tony.rod.bcn@gmail.com>
2025-06-30 17:41:55 +0200

    Added wall collision, sideways collision, code cleanup and object encapsulation

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,49 @@
 // constants
+const startingPos = 34
 const columns = 10
 const rows = 20
 const cellCount = columns * rows
 const cellElements = []
 const startButton = document.getElementById('start-btn')
-const InitialPiece = []
+
+const shapeChoices = ['L', 'Li', 'S', 'Si', 'M', 'O', 'I', 'T'];
+
+const shapes = {
+    L: {
+        className: 'blueShape',
+        coords: [-(columns*2), -columns, 0, 1]
+    },
+    Li: {
+        className: 'redShape',
+        coords: [-(columns*2) + 1, -(columns) + 1, 0, 1]
+    },
+    S: {
+        className: 'greenShape',
+        coords: [-(columns*2), -columns, -(columns) + 1, 1]
+    },
+    Si: {
+        className: 'yellowShape',
+        coords: [-(columns*2) + 1, -(columns) +1, -columns, 0]
+    },
+    M: {
+        className: 'purpleShape',
+        coords: [-(columns*2), -columns, -(columns*2 + 1), 0]
+    },
+    O: {
+        className: 'orangeShape',
+        coords: [-(columns*2), -(columns*2) +1, -(columns), -(columns) +1]
+    },
+    I: {
+        className: 'pinkShape',
+        coords: [-(columns*2), -columns, 0, columns, columns*2]
+    },
+    T: {
+        className: 'magicShape',
+        coords: [-(columns*2), -(columns*2) +1, -(columns*2) +2, -columns +1, 1]
+    }
+};
 
 const scoreDisplay = document.getElementById("score")
-const startBtn = document.getElementById("start")
 const gameOverDisplay = document.getElementById("gameOver")
 const toggleProjectionBtn = document.getElementById("toggleProjection")
 const toggleTPiecesBtn = document.getElementById("toggleTPieces")
@@ -15,157 +51,25 @@ const toggleTPiecesBtn = document.getElementById("toggleTPieces")
 
 
 //variables
+let currentShape = 'L';
+let currentPos = startingPos;
 let interval = null
-let occupied = []
-let board = ''
-let socre = 0
+let score = 0
 let highscore = 0
-let randomPiece = 0
-let shapeNum = 0
-let currentPos = 24;
-let currentShapeNum = 0;
-let cell = 0
+
 
 //functions
 
-function drawShape(currentPos,shapeNum) {
-    if (shapeNum === 0) {
-        Shape0(currentPos)
-    }
-    if (shapeNum === 1) {
-        Shape1(currentPos)
-    }
-    if (shapeNum === 2) {
-        Shape2(currentPos)
-    }
-    if (shapeNum === 3) {
-        Shape3(currentPos)
-    }
-    if (shapeNum === 4) {
-        Shape4(currentPos)
-    } 
-    if (shapeNum === 5) {
-        Shape5(currentPos)
-    }
-    if (shapeNum === 6) {
-        Shape6(currentPos)
-    } 
-    if (shapeNum === 7) {
-        Shape7(currentPos)
+function drawShape() {
+    for (let position of shapes[currentShape].coords) {
+        cellElements[position + currentPos].classList.add(shapes[currentShape].className);
     }
 }
 
-// Blue "L" Shape:
-function Shape0(currentPos) {
-    cellElements[currentPos].classList.add("blueShape");
-    cellElements[currentPos - 10].classList.add("blueShape");
-    cellElements[currentPos - 20].classList.add("blueShape");
-    cellElements[currentPos + 1].classList.add("blueShape");
-}
-// Red "inverted L" Shape:
-function Shape1(currentPos) {
-    cellElements[currentPos + 1].classList.add("redShape");
-    cellElements[currentPos - 9].classList.add("redShape");
-    cellElements[currentPos - 19].classList.add("redShape");
-    cellElements[currentPos].classList.add("redShape");
-}
-// Green "S" Shape:
-function Shape2(currentPos) {
-    cellElements[currentPos + 1].classList.add("greenShape");
-    cellElements[currentPos - 9].classList.add("greenShape");
-    cellElements[currentPos - 10].classList.add("greenShape");
-    cellElements[currentPos - 20].classList.add("greenShape");
-}
-// Yellow "inverted S" Shape:
-function Shape3(currentPos) {
-    cellElements[currentPos].classList.add("yellowShape");
-    cellElements[currentPos - 10].classList.add("yellowShape");
-    cellElements[currentPos - 9].classList.add("yellowShape");
-    cellElements[currentPos - 19].classList.add("yellowShape");
-}
-// Purple "Mountain" Shape:
-function Shape4(currentPos) {
-    cellElements[currentPos].classList.add("purpleShape");
-    cellElements[currentPos - 10].classList.add("purpleShape");
-    cellElements[currentPos - 20].classList.add("purpleShape");
-    cellElements[currentPos - 9].classList.add("purpleShape");
-}
-// Orange "O" Shape:
-function Shape5(currentPos) {
-    cellElements[currentPos - 20].classList.add("orangeShape");
-    cellElements[currentPos - 10].classList.add("orangeShape");
-    cellElements[currentPos - 9].classList.add("orangeShape");
-    cellElements[currentPos - 19].classList.add("orangeShape");
-}
-// Pink "I" Shape:
-function Shape6(currentPos) {
-    cellElements[currentPos - 20].classList.add("pinkShape");
-    cellElements[currentPos - 10].classList.add("pinkShape");
-    cellElements[currentPos + 10].classList.add("pinkShape");
-    cellElements[currentPos].classList.add("pinkShape");
-    cellElements[currentPos + 20].classList.add("pinkShape");
-}
-// Magic "T" Shape:
-function Shape7(currentPos) {
-    cellElements[currentPos].classList.add("magicShape");
-    cellElements[currentPos - 10].classList.add("magicShape");
-    cellElements[currentPos - 19].classList.add("magicShape");
-    cellElements[currentPos - 20].classList.add("magicShape");
-    cellElements[currentPos - 21].classList.add("magicShape");
-}
-
-function currentShapeCollisionPoints(currentPos,shapeNum) {
-    if (shapeNum === 0) {
-        return [currentPos + 10, currentPos, currentPos - 10, currentPos + 11]
-    }
-    if (shapeNum === 1) {
-        return [currentPos + 11, currentPos + 1, currentPos -9, currentPos + 10]
-    }
-    if (shapeNum === 2) {
-        return [currentPos + 11, currentPos + 1, currentPos, currentPos -10]
-    }
-    if (shapeNum === 3) {
-        return [currentPos + 10, currentPos, currentPos +1, currentPos - 9]
-    }
-    if (shapeNum === 4) {
-        return [currentPos + 10, currentPos, currentPos - 10, currentPos + 1]
-    }
-    if (shapeNum === 5) {
-        return [currentPos - 10, currentPos, currentPos + 1, currentPos -9]
-    }
-    if (shapeNum === 6) {
-        return [currentPos - 10, currentPos, currentPos + 20, currentPos + 10, currentPos + 30]
-    }
-    if (shapeNum === 7) {
-        return [currentPos + 10, currentPos, currentPos -9, currentPos -10, currentPos -11]
-    }
-    console.log(currentShapeCollisionPoints)
-}
-
-function currentShapeCollisionPointsSideways(currentPos,shapeNum) {
-    if (shapeNum === 0) {
-        return [currentPos, currentPos - 10, currentPos - 20, currentPos + 1]
-    }
-    if (shapeNum === 1) {
-        return [currentPos + 1, currentPos + -9, currentPos -19, currentPos]
-    }
-    if (shapeNum === 2) {
-        return [currentPos + 1, currentPos - 9, currentPos - 10, currentPos - 20]
-    }
-    if (shapeNum === 3) {
-        return [currentPos, currentPos - 10, currentPos - 9, currentPos - 19]
-    }
-    if (shapeNum === 4) {
-        return [currentPos, currentPos - 10, currentPos - 20, currentPos - 9]
-    }
-    if (shapeNum === 5) {
-        return [currentPos - 20, currentPos -10, currentPos - 9, currentPos - 19]
-    }
-    if (shapeNum === 6) {
-        return [currentPos - 20, currentPos -10, currentPos + 10, currentPos, currentPos + 20]
-    }
-    if (shapeNum === 7) {
-        return [currentPos, currentPos - 10, currentPos - 19, currentPos - 20, currentPos - 21]
+function clearLastShape() {
+    for (let pos of shapes[currentShape].coords) {
+        let cell = cellElements[currentPos + pos];
+        if (cell) cell.classList.remove(shapes[currentShape].className);
     }
 }
 
@@ -173,24 +77,10 @@ function gameOver() {
     clearInterval(interval);
     alert("Game Over");
 }
-function getAllOccupiedIndexes() {
-    const occupied = [];
-    for (let i = 0; i < cellElements.length; i++) {
-        if (cellElements[i].classList.contains("occupied")) {
-            occupied.push(i); 
-        }
-        
-    }
-    occupied.push (210, 211, 212, 213, 214, 215, 216, 217, 218, 219);
-    return occupied;
-}
 
-function stoppedShape(currentPos,currentShapeNum) {
-    const occupied = currentShapeCollisionPoints(currentPos,currentShapeNum)
-    for (let i = 0; i < cellElements.length; i++) {
-        if (occupied.includes(i)) {
-            cellElements[i].classList.add("occupied")
-        }
+function stoppedShape() {
+    for (let pos of shapes[currentShape].coords) {
+        cellElements[currentPos + pos].classList.add('occupied');
     }
 }
 
@@ -214,100 +104,69 @@ function createBoard() {
         cellElements.push(cell)
         board.appendChild(cell)
     }
-    console.log(cellElements)
-    console.log(cellCount)
 }
 
 function createRandomShape() {
-    const shapeNum = Math.floor(Math.random() * 8);
-    currentShapeNum = shapeNum
+    const idx = Math.floor(Math.random() * shapeChoices.length);
+    currentShape = shapeChoices[idx];
 }
 
-function clearLastShape(currentPos,shapeNum) {
-    if (shapeNum === 0) {
-        cellElements[currentPos].classList.remove("blueShape")
-        cellElements[currentPos -10 ].classList.remove("blueShape")
-        cellElements[currentPos -20].classList.remove("blueShape")
-        cellElements[currentPos +1].classList.remove("blueShape")
+function downCollision() {
+    const coords = shapes[currentShape].coords;
+    for (let i = 0; i < coords.length; i++) {
+        const cellIdx = currentPos + coords[i];
+        const cellBelowIdx = cellIdx + columns;
+        if (cellBelowIdx >= cellCount || 
+            (cellElements[cellBelowIdx] && cellElements[cellBelowIdx].classList.contains('occupied'))) {
+            return true;
+        }
     }
-    if (shapeNum === 1) {
-        cellElements[currentPos].classList.remove("redShape")
-        cellElements[currentPos -9].classList.remove("redShape")
-        cellElements[currentPos -19].classList.remove("redShape")
-        cellElements[currentPos +1].classList.remove("redShape")
-    }
-    if (shapeNum === 2) {
-        cellElements[currentPos +1].classList.remove("greenShape")
-        cellElements[currentPos -9].classList.remove("greenShape")
-        cellElements[currentPos -10].classList.remove("greenShape")
-        cellElements[currentPos -20].classList.remove("greenShape")
-    }
-    if (shapeNum === 3) {
-        cellElements[currentPos].classList.remove("yellowShape")
-        cellElements[currentPos -10].classList.remove("yellowShape")
-        cellElements[currentPos -9].classList.remove("yellowShape")
-        cellElements[currentPos -19].classList.remove("yellowShape")
-    }
-    if (shapeNum === 4) {
-        cellElements[currentPos].classList.remove("purpleShape")
-        cellElements[currentPos -10].classList.remove("purpleShape")
-        cellElements[currentPos -20].classList.remove("purpleShape")
-        cellElements[currentPos -9].classList.remove("purpleShape")
-    }
-    if (shapeNum === 5) {
-        cellElements[currentPos -20].classList.remove("orangeShape")
-        cellElements[currentPos -10].classList.remove("orangeShape")
-        cellElements[currentPos -9].classList.remove("orangeShape")
-        cellElements[currentPos -19].classList.remove("orangeShape")
-    }
-    if (shapeNum === 6) {
-        cellElements[currentPos -20].classList.remove("pinkShape")
-        cellElements[currentPos -10].classList.remove("pinkShape")
-        cellElements[currentPos +10].classList.remove("pinkShape")
-        cellElements[currentPos].classList.remove("pinkShape")
-        cellElements[currentPos +20].classList.remove("pinkShape")
-    }
-    if (shapeNum === 7) {
-        cellElements[currentPos].classList.remove("magicShape")
-        cellElements[currentPos -10].classList.remove("magicShape")
-        cellElements[currentPos -19].classList.remove("magicShape")
-        cellElements[currentPos -20].classList.remove("magicShape")
-        cellElements[currentPos -21].classList.remove("magicShape")
-    }
-}
-
-function collision() {
-    const shapeCollisionPoints = currentShapeCollisionPoints(currentPos,currentShapeNum)
-    const occupiedIndexes = getAllOccupiedIndexes()
-    return shapeCollisionPoints.some(index => occupiedIndexes.includes(index + columns)) //needed help for the +columns constant
+    return false;
 }
 
 
-function startAgain() {
-    currentPos = 24
-    createRandomShape()
-    drawShape(currentPos,currentShapeNum);
-    if (collision()) {
-        currentPos = 24
-        drawShape(currentPos,currentShapeNum);
-        stoppedShape(currentPos,currentShapeNum)
-        setTimeout(gameOver, 50);
+function leftCollision() {
+    const coords = shapes[currentShape].coords; 
+    for (let i = 0; i < coords.length; i++) {
+        const cellIdx = currentPos + coords[i];        
+        const cellLeftToIdx = cellIdx - 1;        
+        if (cellElements[cellLeftToIdx].classList.contains('occupied') && (cellIdx % columns !== 0)) { 
+            return true; 
+        }
     }
+    return false; 
+}
+
+function rightCollision() {
+    const coords = shapes[currentShape].coords; 
+    for (let i = 0; i < coords.length; i++) {
+        const cellIdx = currentPos + coords[i];        
+        const cellRightToIdx = cellIdx + 1;        
+        if (cellElements[cellRightToIdx].classList.contains('occupied') && (cellIdx % columns !== 9)) { 
+            return true; 
+        }
+    }
+    return false; 
 }
 
 
-
-function startPlayDown() { 
-    if (collision ()) { 
+function startPlayDown() {
+    if (downCollision()) {
         clearInterval(interval);
-        stoppedShape(currentPos,currentShapeNum)
-        getAllOccupiedIndexes()
-        startAgain()
-        startInterval()
+        stoppedShape();
+        currentPos = startingPos;
+        createRandomShape();
+        if (downCollision()) {
+            drawShape();
+            setTimeout(gameOver, 50);
+            return;
+        }
+        drawShape();
+        startInterval();
     } else {
-        clearLastShape(currentPos,currentShapeNum)
-        currentPos += 10
-        drawShape(currentPos,currentShapeNum)
+        clearLastShape();
+        currentPos += columns;
+        drawShape();
     }
 }
 
@@ -315,24 +174,46 @@ function startInterval() {
         interval = setInterval(startPlayDown, 200)
 }
 
+
+
 function moveShape(event) {
     const keypressed = event.key
-    if (keypressed === "ArrowLeft" && currentPos % columns !== 0) {
-        clearLastShape(currentPos,currentShapeNum)  
-        currentPos--
-        drawShape(currentPos,currentShapeNum)
-        }   
-    if (keypressed === "ArrowRight" && currentPos % columns !== 9) {
-        clearLastShape(currentPos,currentShapeNum)
-        currentPos++
-        drawShape(currentPos,currentShapeNum)
+    let canMove = true
+    if (keypressed === "ArrowLeft" && leftCollision() === false) {
+        for (let i = 0; i < shapes[currentShape].coords.length; i++) {
+            const idx = currentPos + shapes[currentShape].coords[i];
+            if (idx % columns === 0) {
+                canMove = false;
+                break;
+            }
+        }
+        if (canMove) {
+        clearLastShape();
+        currentPos--;
+        drawShape();
+        }
+    }
+    if (keypressed === "ArrowRight" && rightCollision() === false ) {
+        for (let i = 0; i < shapes[currentShape].coords.length; i++) {
+            const idx = currentPos + shapes[currentShape].coords[i];
+            if (idx % columns === 9) {
+                canMove = false;
+                break;
+            }
+        }
+        if (canMove) {
+            clearLastShape();
+            currentPos++;
+            drawShape();
+        }
     }
 }
+
 function startGameFunction() {
     createMainBoard()
     createBoard()
     createRandomShape()
-    drawShape(currentPos,currentShapeNum);
+    drawShape();
     startInterval()
 }
 
